f0e07adcc0173e24d8ff7dd32c74dc3a9098ebca
Rico Herwig <rherwig4711@gmail.com>
2017-09-29 01:17:27 +0200

    implements streaming

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,30 @@ import flushChunks from 'webpack-flush-chunks';
 
 import App from '../shared/App';
 
+const getPageStart = (styles) => `
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    ${styles}
+
+    <title>React 16 | Sample</title>
+</head>
+<body>
+
+`;
+
+const getPageEnd = (cssHash, js) => `
+${cssHash}
+${js}
+
+</body>
+</html>
+`;
+
 /**
  * Provides the server side rendered app. In development environment, this method is called by
  * `react-hot-server-middleware`.
@@ -12,18 +36,15 @@ import App from '../shared/App';
  * @param clientStats Parameter passed by hot server middleware
  */
 export default ({ clientStats }) => async (req, res, next) => {
-    const app = (
-        <App/>
-    );
-
-    const appString = ReactDOM.renderToString(app);
+    const stream = ReactDOM.renderToNodeStream(<App/>);
     const chunkNames = flushChunkNames();
     const { js, styles, cssHash } = flushChunks(clientStats, { chunkNames });
 
-    res.render('index', {
-        appString,
-        js,
-        styles,
-        cssHash
+    res.write(getPageStart(styles));
+    res.write('<div id="react-root">');
+    stream.pipe(res, { end: false });
+    stream.on('end', () => {
+        res.write('</div>');
+        res.end(getPageEnd(cssHash, js));
     });
 };
