9a89c407c9aa4d55d008ad4313feb8c26055b3bb
Cristian Tountzis <cristian.tountzis@gmail.com>
2018-02-02 19:04:54 +1100

    Added server and client config with pages to demo routing.

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/server';
 import { flushChunkNames } from 'react-universal-component/server';
 import flushChunks from 'webpack-flush-chunks';
 
+import { StaticRouter } from 'react-router';
+
 import App from '../shared/App';
 
 /**
@@ -14,18 +16,35 @@ import App from '../shared/App';
  * @param clientStats Parameter passed by hot server middleware
  */
 export default ({ clientStats }) => async (req, res) => {
+    const context = {};
+
     const app = (
-        <App/>
+        <StaticRouter
+          location={req.url}
+          context={context}>
+            <App/>
+        </StaticRouter>
     );
 
     const appString = ReactDOM.renderToString(app);
     const chunkNames = flushChunkNames();
     const { js, styles, cssHash } = flushChunks(clientStats, { chunkNames });
 
-    res.render('index', {
-        appString,
-        js,
-        styles,
-        cssHash
-    });
+    /*
+     * See https://reacttraining.com/react-router/web/guides/server-rendering for details
+     * on this configuration.
+     */
+    if (context.url) {
+        res.writeHead(301, {
+            Location: context.url
+        });
+        res.end();
+    } else {
+        res.render('index', {
+            appString,
+            js,
+            styles,
+            cssHash
+        });
+    }
 };
